890d1ff7019eccbab5437b2943235d705ea211b6
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-04-10 22:00:53 -0400

    Updated js and package.json.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,31 +1,20 @@
 const notes = require("../db/db.json");
 const fs = require("fs");
 const path = require('path');
-const { v4 } = require("uuidv4");
-const { request } = require("http");
+const uniqid = require('uniqid');
 
 module.exports = (app) => {
-    const newNote = request.body;
     app.get("/api/notes", (req, res) => res.json(notes));
 
     app.post('/api/notes', (req, res) => {
-        newNote.id = v4();
-        notes.push(newNote);
+        req.body.id = uniqid();
+        notes.push(req.body);
         fs.writeFile((path.join(__dirname, "../db/db.json")), JSON.stringify(notes), function (error) {
             if (error) throw error;
         })
-        res.json(notes);
+        res.json(true);
     });
-    app.delete("/api/notes/:id", (req, res) => res.json(notes));
     
-    
-    //DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to 
-    //read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-    
-    //add id to each piece of info (uuid package... npm install uuid-v4? )
-    //require uuid-v4 (lookup requirements)
-    //how to join unique id
-    //right before push(line 9) req.body.id = uuidv4()
-    //.filter - research
+    // app.delete("/api/notes/:id", (req, res) => res.json(notes));
 
 }
